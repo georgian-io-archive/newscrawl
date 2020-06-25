a0cfec68b3ee8618e18013aa082fb75f38068a76
Alex Xue <alexxue@MacBook-Pro.local>
2020-06-25 14:03:22 -0400

    add full news feed

--- testthing.py
+++ testthing.py
@@ -21,14 +21,9 @@ from pyspark.sql.functions import udf
 from pyspark.sql.types import StructType, StructField, StringType, LongType, BooleanType
 import argparse
 import spacy
+import pandas as pd
 
 
-parser = argparse.ArgumentParser()
-parser.add_argument("query", help="the key word you want to look at")
-parser.add_argument('--ner', dest='ner', action='store_true')
-parser.add_argument('--no-ner', dest='ner', action='store_false')
-parser.set_defaults(ner=True)
-args = parser.parse_args()
 conf = SparkConf()
 sc = SparkContext(
             appName="asdfasf",
@@ -36,22 +31,59 @@ sc = SparkContext(
 sqlc = SQLContext(sparkContext=sc)
 df = sqlc.read.parquet("spark-warehouse/stringmatchfulltest")
 nlp = spacy.load("en_core_web_sm")
+columns = ['title','body','url','record_date', 'content_length',
+                                 'warc_ip', 'warc_truncated', 'server_name', 'vary',
+                                 'cache_control', 'content_type', 'http_date', 'http_date_expires',
+                                 'html_meta', 'html_hyperlink', 'html_image']
+rows = []
 
 
-def performNER(title, body):
-    doc = nlp(title)
-    for ent in doc.ents:
-        if ent.label_ == "ORG" and ent.text.lower() == args.query.lower():
-            print("org Found:", ent.text)
-            return True
-    return False
 
-performNERudf = udf(performNER, BooleanType())
-df_cols = df.columns
-if args.ner:
-    for row in df.filter(performNERudf("title", "body")).collect():
-        print(row.title)
-else:
-    for row in df.filter(df.title.contains(args.query)).collect():
-        print(row.title)
 
+class CompanyQuery():
+
+    def __init__(self):
+        self.parser = argparse.ArgumentParser()
+        self.parser.add_argument("query", help="the key word you want to look at")
+        self.parser.add_argument('--ner', dest='ner', action='store_true')
+        self.parser.add_argument('--no-ner', dest='ner', action='store_false')
+        self.parser.add_argument('--csv', dest='csv', action='store_true')
+        self.parser.add_argument('--no-csv', dest='csv', action='store_false')
+        self.parser.set_defaults(ner=True)
+        self.parser.set_defaults(csv=False)
+        self.args = self.parser.parse_args()
+
+    def performNER(self,title,body):
+        doc = nlp(title)
+        for ent in doc.ents:
+            if ent.label_ == "ORG" and self.args.query.lower().contains(ent.text.lower()):
+                print("org Found:", ent.text)
+                return True
+        return False
+
+
+    def performQuery(self):
+
+        performNERudf = udf(self.performNER, BooleanType())
+
+        if self.args.ner:
+            for row in df.filter(performNERudf("title", "body")).collect():
+                print(row.title)
+                if self.args.csv:
+                    rows.append([i for i in row])
+        else:
+            for row in df.filter(df.title.contains(self.args.query)).collect():
+                print(row.title)
+                if self.args.csv:
+                    rows.append([i for i in row])
+
+    def saveData(self):
+        print(self.args.csv)
+        if self.args.csv:
+            print("OKO")
+            panda_df = pd.DataFrame(rows,columns=columns)
+          #  print(panda_df)
+            panda_df.to_csv('panda_dataframe.csv')
+temp = CompanyQuery()
+temp.performQuery()
+temp.saveData()
